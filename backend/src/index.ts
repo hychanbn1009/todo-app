687e17f96ef19d2239ca3ce95262c25dc0380ac4
hychanbn1009 <hychanbn1009@gmail.com>
2025-10-26 23:01:55 +0800

    feat: add esbuild and cors to backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,12 +4,29 @@ import todoRoutes from './routes/todoRoutes';
 import fs from 'fs';
 import swaggerUi from 'swagger-ui-express';
 import YAML from 'yaml'
+import cors from 'cors';
 
 const swaggerDocument = YAML.parse(fs.readFileSync('./openapi.yaml', 'utf8'));
 dotenv.config();
 
 const app: Application = express();
 const port = process.env.PORT || 3000;
+
+// CORS configuration
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      const allowedOrigins = ['http://localhost:3001'];
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type'],
+  })
+);
 
 // Middleware to parse JSON
 app.use(express.json());
